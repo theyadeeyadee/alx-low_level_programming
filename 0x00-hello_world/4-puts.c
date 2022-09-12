cda547d4abbc81c397e0d3e552986b51e6bf3de1
Farouk T. Oluyadi <yadeemayana@gmail.com>
2022-09-11 19:20:53 -0700

    hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Printing with puts 
+ *
+ * Return: Always 0 (Checked)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	
+	return (0)
+}
