8913e2d1acdc35fb20c0bae98741ab1c8f4c2107
Farouk T. Oluyadi <yadeemayana@gmail.com>
2022-09-12 02:55:06 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ * main - prints a text with printf
+ *
+ * Return: Always 0 (code checked!)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
