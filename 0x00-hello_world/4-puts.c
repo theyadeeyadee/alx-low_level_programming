dba1a6004ca0faa5459378d9c0fd16cbce06c656
Farouk T. Oluyadi <yadeemayana@gmail.com>
2022-09-12 01:42:41 -0700

    4puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,9 +5,8 @@
  *
  * Return: Always 0 (Checked)
  */
-int main(void);
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	
-	return (0)
+	puts("\"Programming is like building a multilingual puzzle")
+	return (0);
 }
