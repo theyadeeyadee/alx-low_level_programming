e971a918b547920ebbc3cb9305727e00b36659bf
Farouk T. Oluyadi <yadeemayana@gmail.com>
2022-09-12 20:30:29 -0700

    numberx

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers
+ *
+ * Description - using the main function
+ * this program prints all single digit numbers of base 10 starting frm 0
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+	printf("\n");
+	return (0);
+}
