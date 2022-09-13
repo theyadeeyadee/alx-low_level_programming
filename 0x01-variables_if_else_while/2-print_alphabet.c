c7df821cd424f8d005be24f6279bc13e86fe48b3
Farouk T. Oluyadi <yadeemayana@gmail.com>
2022-09-12 19:25:43 -0700

    putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+/**
+ * main - print alphabets
+ *
+ * Description - using the main function
+ * this program prints "Programming is positive, zero, or negative"
+ * Return: 0
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar("\n");
+	return (0);
+}
+
