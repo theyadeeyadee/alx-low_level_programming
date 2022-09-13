46ac899225bfc8a8c1fed4af152a4e60cb121c11
Farouk T. Oluyadi <yadeemayana@gmail.com>
2022-09-12 19:48:11 -0700

    alphaBETS

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - print the alphabets with mixed case
+ *
+ * Description - using the main function
+ * Return: 0
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A' ; ch <= 'Z' ; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
