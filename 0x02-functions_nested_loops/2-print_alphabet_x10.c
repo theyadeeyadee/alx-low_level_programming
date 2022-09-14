9e014a7ec1dde1321ff4919337773f5d379d1000
Farouk T. Oluyadi <yadeemayana@gmail.com>
2022-09-14 15:36:50 -0700

    10x alphabets

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet - function to print abc 10times
+ *
+ * Return: always 0
+ */
+void print_alphabet(void)
+{
+	char c, i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
+
