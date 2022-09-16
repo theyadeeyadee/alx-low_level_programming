d3e1893047ecb650b69726ecae7d70db3799c1ee
Farouk T. Oluyadi <yadeemayana@gmail.com>
2022-09-15 17:56:19 -0700

    Task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- *  * print_most_numbers - entry point
- *   *
- *    * Description: Prints the numbers with _putchar
- *     *
- *      * Return: void
- *       */
+ * print_most_numbers - entry point
+ *
+ * Description: Prints the numbers with _putchar
+ *
+ * Return: void
+ */
 
 void print_most_numbers(void)
 {
 		int number;
-		
+	
 		for (number = '0'; number <= '9'; number++)
 		{
 			if (number == '2' || number == '4')
