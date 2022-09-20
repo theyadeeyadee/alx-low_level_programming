ab20c7651774a68ac747b837a66d52b9a17935d3
Farouk T. Oluyadi <yadeemayana@gmail.com>
2022-09-19 18:37:42 -0700

    1. swap.

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * swap_int- interchanges variables a and b
+ * 
+ * @a:check
+ * @b: checked
+ * 
+ * Return:nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	int swap;
+	
+	swap = *a;
+	
+	*a = *b;
+	
+	*b = swap;
+}
